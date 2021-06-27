b12d3af15895a4ff365ba1018be677e02d5ecb0f
kymaniAnderson <kymanianderson99@gmail.com>
2021-06-27 17:53:57 -0500

    simple auto gen ID

--- client/scripts/addPatient.js
+++ client/scripts/addPatient.js
@@ -1,10 +1,43 @@
 // VARIABLE DECLARATIONS:
 var connectionURL = "http://192.168.100.78:5000"
 
+var count;
+const buffer = 6000000;
+
+window.onload = function() { 
+    getNextID();
+};
+
+function getPatients(){
+    return fetch(connectionURL.concat("/api/patient"))
+    .then((res) => res.json())
+    .then((json) => json);
+}
+
+async function getNextID(){
+    let patients = await getPatients();
+    let maxID = buffer-1;
+
+    patients.forEach((patient) => {
+        idToInt = parseInt(patient.patientID);
+
+        if(idToInt > maxID){
+           maxID = idToInt;
+           count = maxID + 1;
+        }
+    });    
+}
+
 document.getElementById("patient-submit").addEventListener("click", function(event){
     event.preventDefault(); 
 
-    let patientID = document.getElementById("patientID").value;
+    if(count){
+        patientID = count;
+    }
+    else{
+        patientID = buffer;
+    }
+
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
     let patientAge = document.getElementById("patientAge").value;
@@ -13,7 +46,7 @@ document.getElementById("patient-submit").addEventListener("click", function(eve
         "firstName": firstName,
         "lastName": lastName,
         "patientAge": patientAge,
-        "patientID": patientID
+        "patientID": patientID.toString()
     };
 
     fetch(connectionURL.concat("/api/patient"), {
